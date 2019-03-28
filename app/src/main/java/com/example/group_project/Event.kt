c919ee1cca19a837e173de53518768e727647024
saiajay5674 <saiajay5674@gmail.com>
2019-03-28 11:16:51 -0400

    Created EventListAdapter and Event Class

---
+++ app/src/main/java/com/example/group_project/Event.kt
@@ -0,0 +1,3 @@
+package com.example.group_project
+
+class Event (val id: String, val sport: String, val time: String, val location : String, val players: Int)
--- app/src/main/java/com/example/group_project/EventsListFragment.kt
+++ app/src/main/java/com/example/group_project/EventsListFragment.kt
@@ -6,12 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -24,7 +21,46 @@ class EventsListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events_list, container, false)
+        var view = inflater.inflate(R.layout.fragment_events_list, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.events_list)
+        val adapter = EventListAdapter()
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        return view
+    }
+
+    inner class EventListAdapter():
+        RecyclerView.Adapter<EventListAdapter.EventViewHolder>() {
+
+        var events = emptyList<Event>()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
+
+            val v = LayoutInflater.from(parent.context)
+                .inflate(R.layout.card_view, parent, false)
+            return EventViewHolder(v)
+        }
+
+        override fun getItemCount(): Int {
+
+            return events.size
+        }
+
+        override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
+
+        }
+
+        inner class EventViewHolder(val view: View): RecyclerView.ViewHolder(view), View.OnClickListener{
+            override fun onClick(view: View?){
+
+                if (view != null) {
+
+                }
+            }
+        }
     }
 
 
