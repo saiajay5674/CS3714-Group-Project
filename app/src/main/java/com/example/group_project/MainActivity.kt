79411a0576166f9c2a290a6bb41d20f724e3b211
saiajay5674 <saiajay5674@gmail.com>
2019-03-27 20:16:35 -0400

    Split Into Fragments

--- app/src/main/java/com/example/group_project/MainActivity.kt
+++ app/src/main/java/com/example/group_project/MainActivity.kt
@@ -2,42 +2,67 @@ package com.example.group_project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(){
 
-    private lateinit var mMap: GoogleMap
+    //private lateinit var fragment: Fragment
+    //private lateinit var mMap: GoogleMap
+    private var mBottomNav: BottomNavigationView? = null
+
+    private val listener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+
+        when(item.itemId) {
+
+            R.id.menu_new_event -> {
+                val mapFragment = MapFragment.newInstance()
+                openFragment(mapFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.menu_events -> {
+                val eventsListfragment = EventsListFragment()
+                openFragment(eventsListfragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.menu_profile -> {
+                val profileFragment = ProfileFragment()
+                openFragment(profileFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+
+        false
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_maps)
+        setContentView(R.layout.activity_main)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mBottomNav = findViewById(R.id.bottom_navigation)
+        mBottomNav?.setOnNavigationItemSelectedListener(listener)
+
+
 
     }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+
 }
