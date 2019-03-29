8ae249c1e7a807a25810d900ad885b2b54429e7a
leoul <leoul@Leouls-MacBook-Pro.local>
2019-03-29 18:10:27 -0400

    added current location

--- app/src/main/java/com/example/group_project/MapFragment.kt
+++ app/src/main/java/com/example/group_project/MapFragment.kt
@@ -1,7 +1,13 @@
 package com.example.group_project
 
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -9,98 +15,172 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
-
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class MapFragment : Fragment(), OnMapReadyCallback{
+class MapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
-//    private val mapCallBack = OnMapReadyCallback {
-//        mMap = it
-//
-//        // Add a marker in Sydney and move the camera
-//        val sydney = LatLng(-34.0, 151.0)
-//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-//    }
+    private var latitude = 0.toDouble()
+    private var longitude = 0.toDouble()
+
+    private lateinit var lastLocation: Location
+    private var marker: Marker? = null
+
+    //Location
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var locationCallback: LocationCallback
+
+    companion object {
+        private const val PERMISSION_CODE: Int = 1000
+    }
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_map, container, false)
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
 
         mapFragment.getMapAsync(this)
-        // Inflate the layout for this fragment
 
 
-    (view.findViewById(R.id.popBtn) as Button).setOnClickListener{
+        //Request runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkLocationPermission()) {
+                buildLocationRequest()
+                buildLocationCallBack()
 
+                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+            }
+        } else {
+            buildLocationRequest()
+            buildLocationCallBack()
 
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper())
+        }
 
-        val dialog = Dialog()
+        (view.findViewById(R.id.popBtn) as Button).setOnClickListener {
+            val dialog = Dialog()
 
-        //dialog.setTargetFragment(this,1)
-        dialog.show(childFragmentManager, "dialog")
+            //dialog.setTargetFragment(this,1)
+            dialog.show(childFragmentManager, "dialog")
+        }
 
-        //val builder = AlertDialog.Builder(context)
-//        // Get the layout inflater
-//        val inflater = requireActivity().layoutInflater;
-//
-//        // Inflate and set the layout for the dialog
-//        // Pass null as the parent view because its going in the dialog layout
-//        builder.setView(inflater.inflate(R.layout.popup, null))
-//            // Add action buttons
-//            .setPositiveButton("dd",
-//                DialogInterface.OnClickListener { dialog, id ->
-//                    // sign in the user ...
-//                })
-//            .setNegativeButton("cancel",
-//                DialogInterface.OnClickListener { dialog, id ->
-//                    dialog.cancel()
-//                })
-//        builder.create()
-//     ?: throw IllegalStateException("Activity cannot be null")
+        val spinner: Spinner? = (getView()?.findViewById(R.id.add_event_sport) as? Spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+
+        ArrayAdapter.createFromResource(context, R.array.games, android.R.layout.simple_spinner_item).also { adapter ->
+
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            // Apply the adapter to the spinner
+            spinner?.adapter = adapter
+        }
+
+        return view
     }
 
-//    (view.findViewById(R.id.timePick) as? Button).setOnClickListener{
-//        val calander = Calendar.getInstance()
-//
-//        val selectedTimeListner = TimePickerDialog.OnTimeSetListener{timePicker, hour, minute ->
-//            calander.set(Calendar.HOUR_OF_DAY, hour)
-//            calander.set(Calendar.MINUTE, minute)
-//
-//           timeWindow.text = SimpleDateFormat("HH:mm").format(calander.time)
-//        }
-//        TimePickerDialog(context,selectedTimeListner,calander.get(Calendar.HOUR_OF_DAY), calander.get(Calendar.MINUTE),true).show()
-//
-//    }
+    private fun buildLocationCallBack() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                lastLocation = p0!!.locations.get(p0!!.locations.size - 1) // get last location
 
-    val spinner: Spinner?= (getView()?.findViewById(R.id.add_event_sport) as? Spinner)
-// Create an ArrayAdapter using the string array and a default spinner layout
-    ArrayAdapter.createFromResource(context, R.array.games, android.R.layout.simple_spinner_item).also { adapter ->
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        // Apply the adapter to the spinner
-        spinner?.adapter = adapter
+                if (marker != null) {
+                    marker!!.remove()
+                }
+
+                latitude = lastLocation.latitude
+                longitude = lastLocation.longitude
+
+                val latLng = LatLng(latitude, longitude)
+
+                var markerOptions = MarkerOptions()
+                    .position(latLng)
+                    .title("current Position")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED))
+
+                marker = mMap.addMarker((markerOptions))
+
+                //Move Camera
+                mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+
+                // mMap!!.animateCamera(CameraUpdateFactory.zoomTo(11f))
+                mMap!!.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11f))
+            }
+        }
     }
 
+    @SuppressLint("RestrictedApi")
+    private fun buildLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 3000
+        locationRequest.smallestDisplacement = 10f
+    }
 
-    return view
+    private fun checkLocationPermission(): Boolean {
+
+
+        if (ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity!!,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION))
+
+                ActivityCompat.requestPermissions(activity!!, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    , PERMISSION_CODE)
+
+            else
+                ActivityCompat.requestPermissions(activity!!, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    , PERMISSION_CODE)
+
+            return false
+        } else
+            return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED)
+
+                        if (checkLocationPermission()) {
+
+                            buildLocationRequest()
+                            buildLocationCallBack()
+
+                            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!)
+                            fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback,
+                                Looper.myLooper())
+
+                            mMap!!.isMyLocationEnabled = true
+                        }
+                } else {
+                    Toast.makeText(context!!, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     /**
@@ -115,11 +195,26 @@ class MapFragment : Fragment(), OnMapReadyCallback{
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //Initialize GooglePlay Services
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+                mMap!!.isMyLocationEnabled = false // true??
+            }
+        } else
+
+            //TODO: move camera automatically when location is enabled
+            //no Effect???
+           mMap!!.isMyLocationEnabled = true
+
+        //Enable zoom in and out control
+        mMap.uiSettings.isZoomControlsEnabled = true
+
 
     }
 
+    override fun onStop() {
+//        fusedLocationProviderClient?.removeLocationUpdates(locationCallback)
+        super.onStop()
+    }
 }
