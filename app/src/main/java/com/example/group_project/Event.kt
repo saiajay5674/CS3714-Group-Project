774f6da4d3ed131932d28c8d8705c81c0507bfc3
saiajay5674 <saiajay5674@gmail.com>
2019-04-23 21:49:49 -0400

    Implemented Host Name info on event

--- app/src/main/java/com/example/group_project/Event.kt
+++ app/src/main/java/com/example/group_project/Event.kt
@@ -1,12 +1,13 @@
 package com.example.group_project
 
-class Event (event_id: String, sport: String, time: String, location : String, players: String) {
+class Event (event_id: String, sport: String, time: String, location : String, players: String, host: String) {
 
     var event_id: String
     var sport: String
     var time: String
     var location : String
     var players: String
+    var host: String
 
     init {
         this.event_id = event_id
@@ -14,7 +15,8 @@ class Event (event_id: String, sport: String, time: String, location : String, p
         this.location = location
         this.time = time
         this.players = players
+        this.host = host
     }
 
-    constructor() : this("","","","","")
+    constructor() : this("","","","","", "")
 }
--- app/src/main/java/com/example/group_project/EventDialog.kt
+++ app/src/main/java/com/example/group_project/EventDialog.kt
@@ -2,32 +2,39 @@ package com.example.group_project
 
 import `in`.madapps.placesautocomplete.PlaceAPI
 import `in`.madapps.placesautocomplete.adapter.PlacesAutoCompleteAdapter
+import `in`.madapps.placesautocomplete.model.Place
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.widget.AutoCompleteTextView
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
 import androidx.fragment.app.DialogFragment
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.seatgeek.placesautocomplete.PlacesAutocompleteTextView
+import kotlinx.android.synthetic.main.activity_login.*
 
 
 class EventDialog: DialogFragment() {
 
     private var placesAutocomplete : AutoCompleteTextView? = null
-    var location: AutoCompleteTextView? = null
+    private lateinit var firebaseAuth: FirebaseAuth
 
-    //val placesApi = PlaceAPI.Builder().apiKey("AIzaSyAeMzjhWrb6ZCLmhkqzelmY6LD63e2_VPY").build()
+    lateinit var mainActivity:MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
-
 
             val builder = AlertDialog.Builder(it)
             // Get the layout inflater
@@ -35,9 +42,36 @@ class EventDialog: DialogFragment() {
             val view = inflater.inflate(R.layout.popup, null)
             // Inflate and set the layout for the dialog
             // Pass null as the parent view because its going in the dialog layout
+            firebaseAuth = FirebaseAuth.getInstance()
+            val userID = firebaseAuth.currentUser?.uid
 
-//            location = view?.findViewById(R.id.event_location)
-//            location?.setAdapter(PlacesAutoCompleteAdapter(activity!!.applicationContext, placesApi))
+            val placesApi = PlaceAPI.Builder().apiKey("AIzaSyAeMzjhWrb6ZCLmhkqzelmY6LD63e2_VPY").build(mainActivity)
+
+            placesAutocomplete = view?.findViewById(R.id.event_location)
+            placesAutocomplete?.setAdapter(PlacesAutoCompleteAdapter(activity!!.applicationContext, placesApi))
+
+            placesAutocomplete?.onItemClickListener =
+                AdapterView.OnItemClickListener { parent, _, position, _ ->
+                    val place = parent.getItemAtPosition(position) as Place
+                    placesAutocomplete?.setText(place.description)
+                }
+
+            val ref = FirebaseDatabase.getInstance().getReference("users/" + userID)
+
+            var user: User? = null
+
+            ref.addValueEventListener(object: ValueEventListener {
+                override fun onCancelled(p0: DatabaseError?) {
+                    //Does nothing
+                }
+
+                override fun onDataChange(p0: DataSnapshot?) {
+
+                    user = p0?.getValue(User::class.java)
+
+                }
+
+            })
 
             builder.setView(view)
                 // Add action buttons
@@ -47,20 +81,18 @@ class EventDialog: DialogFragment() {
 
                     val sport = view?.findViewById<Spinner>(R.id.add_event_sport)?.selectedItem.toString()
                     val time = view?.findViewById<EditText>(R.id.add_event_time)?.text.toString()
-                    //val location = view?.findViewById<EditText>(R.id.event_location)?.text.toString()
+                    val location = view?.findViewById<EditText>(R.id.event_location)?.text.toString()
                     val players = view?.findViewById<EditText>(R.id.add_event_players_number)?.text.toString()
 
-
-                    val placeName = location!!.toString()
                     val ref = FirebaseDatabase.getInstance().getReference("events")
-
+                    val host = user?.username
                     val eventId = ref.push().key
 
-                    val event = Event(eventId, sport, time, placeName, players)
+                    val event = Event(eventId, sport, time, location, players, host!! )
 
                     ref.child(eventId).setValue(event).addOnCompleteListener {
 
-                        //Toast.makeText(activity, "Your event has been added", Toast.LENGTH_LONG).show()
+                        Toast.makeText(mainActivity, "Event created", Toast.LENGTH_LONG).show()
                     }
                 }
                 .setNegativeButton("cancel") { dialog, id ->
@@ -70,6 +102,7 @@ class EventDialog: DialogFragment() {
 
         } ?: throw IllegalStateException("Activity cannot be null")
     }
+
 
 }
 
