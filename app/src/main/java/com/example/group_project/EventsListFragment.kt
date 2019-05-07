f0a5afa17cd0b96d9740a88d0c7f88def77c617a
saiajay5674 <saiajay5674@gmail.com>
2019-05-06 21:55:09 -0400

    Calculate distance in list fragment

--- app/src/main/java/com/example/group_project/EventsListFragment.kt
+++ app/src/main/java/com/example/group_project/EventsListFragment.kt
@@ -1,21 +1,27 @@
 package com.example.group_project
 
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.*
+import android.widget.*
 import androidx.fragment.app.Fragment
-import android.widget.ImageButton
-import android.widget.LinearLayout
-import android.widget.PopupMenu
-import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.card_view.view.*
 import org.w3c.dom.Text
-
+import java.util.*
 
 
 /**
@@ -25,6 +31,21 @@ import org.w3c.dom.Text
 class EventsListFragment : Fragment() {
     lateinit var recyclerView: RecyclerView
     lateinit var database: FirebaseDatabase
+    lateinit var geocoder: Geocoder
+    lateinit var locationManager: LocationManager
+    lateinit var mainActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+    var latitude: Double? = null
+    var longitude: Double? = null
+
+    companion object {
+        private const val PERMISSION_CODE: Int = 1000
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -33,7 +54,7 @@ class EventsListFragment : Fragment() {
         //val model = activity.run { ViewModelProviders.of(this!!).get(EventViewModel::class.java) }
 
         recyclerView = view.findViewById(R.id.events_list)
-
+        geocoder = Geocoder(context, Locale.getDefault())
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.events_list)
         val adapter = EventListAdapter()
@@ -41,6 +62,8 @@ class EventsListFragment : Fragment() {
 
         recyclerView.adapter = adapter
         recyclerView.layoutManager = LinearLayoutManager(context)
+
+        locationManager = mainActivity.getSystemService(Context.LOCATION_SERVICE) as LocationManager
 
         val ref = database.getReference("events")
 
@@ -66,11 +89,17 @@ class EventsListFragment : Fragment() {
             }
 
         })
-
-
-
-
         return view
+    }
+
+    fun updateUserLocation()
+    {
+        if(checkLocationPermission())
+        {
+            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            latitude = location?.latitude
+            longitude = location?.longitude
+        }
     }
 
     inner class EventListAdapter():
@@ -90,6 +119,31 @@ class EventsListFragment : Fragment() {
             return events.size
         }
 
+        fun getDistance(address: String): Float
+        {
+            val location = geocoder.getFromLocationName(address, 1)[0]
+
+
+            val currentLocation = Location("Current Location")
+
+            updateUserLocation()
+            if(latitude == null || longitude == null || location == null)
+            {
+                return -1f
+            }
+
+            currentLocation.latitude = latitude!!
+            currentLocation.longitude = longitude!!
+
+            val eventLocation = Location("Evnt Location")
+            eventLocation.longitude = location.longitude
+            eventLocation.latitude = location.latitude
+
+
+            return currentLocation.distanceTo(eventLocation) / 1609.34f
+
+        }
+
         override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
 
 
@@ -97,8 +151,11 @@ class EventsListFragment : Fragment() {
             holder.view.findViewById<TextView>(R.id.sport).text = events[position].sport
             holder.view.findViewById<TextView>(R.id.location).text = events[position].location
             holder.view.findViewById<TextView>(R.id.host).text = "Host " + events[position].host.username
+            holder.view.findViewById<TextView>(R.id.distance).text = "Distance " + getDistance(events[position].location).toString() + " mi"
+
             holder.view.itemView.setOnClickListener {
 
+                it.findNavController().navigate(R.id.action_eventsListFragment_to_eventsDisplayFragment)
             }
 
             val uid = FirebaseAuth.getInstance().currentUser?.uid
@@ -162,6 +219,46 @@ class EventsListFragment : Fragment() {
 
                 if (view != null) {
 
+                }
+            }
+        }
+    }
+
+
+    private fun checkLocationPermission(): Boolean {
+
+
+        if (ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity!!,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION))
+
+                ActivityCompat.requestPermissions(activity!!, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    , PERMISSION_CODE)
+
+            else
+                ActivityCompat.requestPermissions(activity!!, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    , PERMISSION_CODE)
+
+            return false
+        } else
+            return true
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(context!!, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED)
+
+                        if (checkLocationPermission()) {
+
+                            return
+                        }
+                } else {
+                    Toast.makeText(context!!, "Permission Denied", Toast.LENGTH_SHORT).show()
                 }
             }
         }
